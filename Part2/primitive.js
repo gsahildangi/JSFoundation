83d34fb575d3f0359abe9e397a400f627e99679e
Sahil Dangi <gsahil0508@gmail.com>
2025-06-01 05:39:24 +0530

    half primitive and non primitive

---
+++ Part2/primitive.js
@@ -0,0 +1,32 @@
+let bal = 120;
+let anotherbal = new Number(120);//not recommended
+
+console.log(bal);
+console.log(anotherbal);
+
+console.log("");
+
+console.log(typeof(bal));
+console.log(typeof(anotherbal));
+
+//Boolean
+
+let isActive = true;  // primitive type
+let isReallyActive = new Boolean(false); // again not recommended but this is convering primitive in non primitive
+
+//null and undefined
+let firstName;
+console.log(firstName); // now this will be undefined that mean it is present and value is not known
+
+let lastName = null ;
+console.log(lastName); // This is a example of null which is that last name has a value and which is null.
+
+// String
+
+let myString = "hello"
+let hisString = 'Hola'
+
+
+
+
+
