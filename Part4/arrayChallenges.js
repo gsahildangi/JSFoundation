b553a4e6f7f37e5fd6648e4498f116c03b02734b
Sahil Dangi <gsahil0508@gmail.com>
2025-06-07 06:02:04 +0530

    array done loops start

--- Part4/arrayChallenges.js
+++ Part4/arrayChallenges.js
@@ -47,7 +47,7 @@ let arrLength = citiesVisited.push("Berlin")  // as push method of array return 
 */
 
 let teaOrders = ["Chai", "Iced Tea", "Matcha", "Earl Grey"]
-console.log(teaOrders); 
+// console.log(teaOrders); 
 let poopedItem = teaOrders.pop(); //Removes the last element from an array and returns it. If the array is empty, undefined is returned and the array is not modified.
 // console.log(teaOrders);
 // console.log(poopedItem);
@@ -61,10 +61,10 @@ let popularTeas = ["Green tea", "Oolong Tea" , "Chai" ]
 let softCopyTeas =[]
 
 softCopyTeas = popularTeas;
-popularTeas.pop();     // when we will check both the array it i
-console.log(popularTeas);
+popularTeas.pop();     // when we will check both the array as when we soft copy the " memory address is same " and once we delete data from one array other data also get deleted.
+// console.log(popularTeas);
 
-console.log(softCopyTeas);
+// console.log(softCopyTeas);
 
 /* 
 7. You have an array named `topCities` containing `"Berlin"`, `"Singapore"`, and `"New York"`. 
@@ -73,4 +73,45 @@ console.log(softCopyTeas);
 
 let topCities = ["Berlin" , "Singapore" , "New York"]
 
+let hardCopyCities = [...topCities]
+topCities.pop();
+// console.log(hardCopyCities);
 
+
+/* 
+8. You have two arrays: `europeanCities` containing `"Paris"` and `"Rome"`, and `asianCities` containing `"Tokyo"` and `"Bangkok"`. 
+   Merge these two arrays into a new array named `worldCities`.
+*/
+
+let europeanCities = ["Paris" , "Rome" ]
+let asianCities = ["Tokyo" , "Bankok"]
+
+//let worldCities = europeanCities + asianCities ;//  This will convert both into a string and will return the string
+
+//let worldCities = [europeanCities  , asianCities] // This will also solve the problem however it will create a structure like [["",""],["",""]]
+
+let worldCities = europeanCities.concat(asianCities); //This is the best way to do this.
+// console.log(worldCities);
+
+
+/* 
+9. You have an array named `teaMenu` containing `"masala chai"`, `"oolong tea"`, `"green tea"`, and `"earl grey"`. 
+   Find the length of the array and store it in a variable named `menuLength`.
+*/
+
+let teaMenu = ["masala chai" , "oolong tea" , "green tea" , "earl grey"]
+
+let menuLength = teaMenu.length;
+
+// console.log(menuLength);
+
+/* 
+10. You have an array named `cityBucketList` containing `"Kyoto"`, `"London"`, `"Cape Town"`, and `"Vancouver"`. 
+    Check if `"London"` is in the array and store the result in a variable named `isLondonInList`.
+*/
+
+let cityBucketList = ["Kyoto" , "London" ,"cape Town" , "Vancour"]
+
+let isLondonInList = cityBucketList.includes("london");
+
+console.log(isLondonInList);
