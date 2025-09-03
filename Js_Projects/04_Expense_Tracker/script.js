b0d9514ab0f08ffc5264a307e8f0ece91b794aef
Sahil Dangi <gsahil0508@gmail.com>
2025-09-04 05:14:14 +0530

    Lenovo done

--- Js_Projects/04_Expense_Tracker/script.js
+++ Js_Projects/04_Expense_Tracker/script.js
@@ -1,12 +1,23 @@
 document.addEventListener('DOMContentLoaded' , () => {
    
  const expenseForm = document.getElementById('expense-form');
- const expenseName = document.getElementById('expense-name');
- const expenseAmount = document.getElementById('expense-amount');
+ const expenseNameInput = document.getElementById('expense-name');
+ const expenseAmountInput = document.getElementById('expense-amount');
+ const expenseList = document.getElementById('expense-list');
  const formButton = document.getElementsByTagName('button');
  const totalAmountDisplay = document.getElementById('total-amount');
 
-formButton.addEventListener('click' , () => {
+ const expenses = []
+// whenever a form is submitted anywhere then all the values of form are 
+//given back as string or when you will check the value of form or get those value
+// with help of expenseAmountInput.value it will come in string.
+
+expenseForm.addEventListener('submit' , (e) => {
+    e.preventDefault();
+    const name = expenseNameInput.value.trim();
+    // console.log(typeof expenseAmountInput.value)// it will be string check the theory above
+    const amount = parseFloat(expenseAmountInput.value.trim())
+    
     
 })
 
